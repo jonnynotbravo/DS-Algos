2458a25e67ce1d966261dbc4a7953790f73dfec1
Jonny Tilahun <Jonnybiruk13@gmail.com>
2023-03-05 00:25:11 -0500

    Half Done! Find Intersection

--- Find Intersection.js
+++ Find Intersection.js
@@ -1,5 +1,5 @@
-// Have the function FindIntersection(strArr) read the array of strings 
-// stored in strArr which will contain 2 elements: the first element will 
+// Have the function FindIntersection(strArr) read the array of strings
+// stored in strArr which will contain 2 elements: the first element will
 // represent a list of comma - separated numbers sorted in ascending order,
 // the second element will represent a second list of comma - separated numbers(also sorted).
 // Your goal is to return a comma - separated string containing the
@@ -13,4 +13,19 @@
 // Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
 // Output: 1,9,10
 
-// Answer 1
+// Answer One
+function FindIntersection(strArr) {
+  let array = [];
+  const first = strArr[0].split(", ");
+  const second = strArr[1].split(", ");
+
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {
+      if (first[i] === second[j]) {
+        array.push(first[i]);
+      }
+    }
+  }
+
+  return array.join(",");
+}
